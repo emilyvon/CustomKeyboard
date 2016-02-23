5471c0e067c651829b698cc85d2271401e72f254
emilyvon <emilyfung86@hotmail.com>
2016-02-24 00:15:28 +1030

    initial commit

---
+++ Custom Keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Custom Keyboard
+//
+//  Created by Mengying Feng on 5/01/2016.
+//  Copyright © 2016 Mengying Feng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HodorBoard/KeyboardViewController.swift
@@ -0,0 +1,81 @@
+//
+//  KeyboardViewController.swift
+//  HodorBoard
+//
+//  Created by Mengying Feng on 5/01/2016.
+//  Copyright © 2016 Mengying Feng. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+    
+        // Add custom view sizing constraints here
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        let button = UIButton(type: .System)
+        button.frame = CGRectMake(150, 40, 100, 120)
+        
+        var hodor = UIImage(named: "minion.png")
+        button.setBackgroundImage(hodor, forState: .Normal)
+        
+        button.addTarget(self, action: "didPressButton", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(button)
+        
+        
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .System)
+    
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), forState: .Normal)
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+    
+        self.nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+    
+        let nextKeyboardButtonLeftSideConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Left, relatedBy: .Equal, toItem: self.view, attribute: .Left, multiplier: 1.0, constant: 0.0)
+        let nextKeyboardButtonBottomConstraint = NSLayoutConstraint(item: self.nextKeyboardButton, attribute: .Bottom, relatedBy: .Equal, toItem: self.view, attribute: .Bottom, multiplier: 1.0, constant: 0.0)
+        self.view.addConstraints([nextKeyboardButtonLeftSideConstraint, nextKeyboardButtonBottomConstraint])
+    }
+
+    func didPressButton(){
+        var proxy = textDocumentProxy as UITextDocumentProxy
+        proxy.insertText("Hodor")
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
+
+    override func textWillChange(textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+
+    override func textDidChange(textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+    
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.Dark {
+            textColor = UIColor.whiteColor()
+        } else {
+            textColor = UIColor.blackColor()
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, forState: .Normal)
+    }
+
+}
